73c913df3dc0f3fdc30487e2daad47062b73894c
FireBird <suyashbakshi301995@gmail.com>
2016-11-29 14:27:18 -0600

    Temperature sensor addition

--- test_mqtt.ino
+++ test_mqtt.ino
@@ -1,9 +1,19 @@
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
 #include <SPI.h>
-#include <WiFi101.h>
 #include <PubSubClient.h>
- 
+
+#define DHTTYPE           DHT22
+#define DHTPIN            12
+#include <WiFi101.h>
+
+DHT_Unified dht(DHTPIN, DHTTYPE);
+
+uint32_t delayMS;
 char message[100];
-int ledPin = 13;
+WiFiClient ethClient;
+PubSubClient mqttClient(ethClient);
  
 void callback(char* topic, byte* payload, unsigned int length) {
 
@@ -17,26 +27,47 @@ void callback(char* topic, byte* payload, unsigned int length) {
     Serial.print((char)payload[i]);
     message[i]=payload[i];
   }
+  
   message[i]='\0';
+  Serial.println();
   String msg = String(message);
 
-  //if message contains request to get temperature, publish the temperature
+  if(msg.equals("Alive?")){
+    mqttClient.publish("status/","I'm Alive");
+  }
+  else if(msg.equals("temp")){
+    //return temperature value
+    sensors_event_t event;  
+    dht.temperature().getEvent(&event);
+//    if (isnan(event.temperature)) {
+//      Serial.println("Error reading temperature!");
+//    }
+//    else {
+     Serial.print("Temperature: ");
+     Serial.println(event.temperature);
+//    }  
+     int t = (int)event.temperature;
+     String curTempStr = String(event.temperature);
+     char buff[10];
+     curTempStr.toCharArray(buff,10);
+     Serial.println(buff);
+     mqttClient.publish("status/",buff);
+  }
+  else{
+    String reqTempStr = msg.substring(5);
+    int reqTemp = reqTempStr.toInt();
+    //Serial.println(reqTemp);
+    Serial.println("REQUIRED TEMPERATURE: " + reqTempStr);
+    delay(reqTemp*1000);
+    
+
+  }
+  
   //look for temp in message,compare it to what is now and turn on fan for that amount of time
   //::To calculate time, invent a formula for amount of time required to keep the fan running.
-  //hint: write a loop since callback will be called only when the message is received(only once)
-  
-   
-  if(msg.equals("on")){
-      digitalWrite(ledPin,HIGH);
-    }
-   else if(msg.equals("off")){
-      digitalWrite(ledPin,LOW);
-    }
-  Serial.println();
+  //hint: write a loop to keep fan running since callback will be called only when the message is received(only once)
+    
 }
- 
-WiFiClient ethClient;
-PubSubClient mqttClient(ethClient);
  
 void reconnect() {
   // Loop until we're reconnected
@@ -46,7 +77,7 @@ void reconnect() {
     if (mqttClient.connect("arduinoClient")) {
       Serial.println("connected");
       // Once connected, publish an announcement...
-      mqttClient.publish("status/","hello world");
+      mqttClient.publish("status/","I'm Alive");
       // ... and resubscribe
       mqttClient.subscribe("control/#");
     } else {
@@ -63,7 +94,10 @@ void setup()
 {
   WiFi.setPins(8,7,4,2);
   Serial.begin(9600);
-  pinMode(ledPin,OUTPUT);
+  dht.begin();
+  sensor_t sensor;
+  dht.temperature().getSensor(&sensor);
+  dht.humidity().getSensor(&sensor);
   
   WiFi.begin("UHWireless");
   delay(10000);
